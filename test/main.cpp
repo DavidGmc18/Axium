b095b07bd02ce682a650c2dc2f4f0c85cd8da858
DavidGmc18 <davidgames.youtub@gmail.com>
2025-11-15 20:23:52 +0100

    add & sub ops

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,27 @@
 #include "AXM/Tensor.hpp"
 #include "AXM/operations.hpp"
+#include "AXM/tensor_util.hpp"
 #include <chrono>
 #include <iostream>
+
+int main() {
+    axm::Tensor<float> a = axm::tensor::full<float>({2, 2}, -1);
+    axm::Tensor<float> b = axm::tensor::zeros<float>({2, 2});
+    axm::Tensor<float> c = axm::tensor::ones<float>({3, 2, 1});
+    axm::Tensor<float> d = axm::tensor::range<float>(1);
+    axm::Tensor<float> e = axm::tensor::range<float>(10, 0, -1.9);
+    axm::Tensor<float> f = axm::tensor::range<float>(10, 0, -1.99);
+    axm::Tensor<float> g(f.descriptor());
+    axm::op::sub<float>(e.data, f.data, g.data, g.size);
+
+    std::cout << a << "\n\n";
+    std::cout << b << "\n\n";
+    std::cout << c << "\n\n";
+    std::cout << d << "\n\n";
+    std::cout << e << "\n\n";
+    std::cout << f << "\n\n";
+    std::cout << g << "\n\n";
+}
 
 // int main() {
 //     const size_t N = 960;
@@ -30,49 +50,49 @@
 // }
 
 
-int main() {
-    const size_t M = 6, N = 16, K = 2;
-    axm::Tensor<float> a({M, K});
-    axm::Tensor<float> b({K, N});
+// int main() {
+//     const size_t M = 6, N = 16, K = 2;
+//     axm::Tensor<float> a({M, K});
+//     axm::Tensor<float> b({K, N});
 
-    axm::Tensor<float> c({M, N});
-    axm::Tensor<float> d({M, N});
+//     axm::Tensor<float> c({M, N});
+//     axm::Tensor<float> d({M, N});
 
-    a = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-    };
+//     a = {
+//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+//     };
 
-    b = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,  
-    };
+//     b = {
+//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,  
+//     };
 
-    float alpha = 1.0f;
-    float beta = 0.0f;
-    axm::op::sgemm(
-        M, N, K,
-        alpha,
-        (float*)a.data, a.strides[0], false,
-        (float*)b.data, b.strides[0], false,
-        beta,
-        (float*)c.data, c.strides[0]
-    );
-    axm::op::sgemm_naive(
-        M, N, K,
-        alpha,
-        (float*)a.data, a.strides[0], false,
-        (float*)b.data, b.strides[0], false,
-        beta,
-        (float*)d.data, d.strides[0]
-    );
+//     float alpha = 1.0f;
+//     float beta = 0.0f;
+//     axm::op::sgemm(
+//         M, N, K,
+//         alpha,
+//         a.data, a.strides[0], false,
+//         b.data, b.strides[0], false,
+//         beta,
+//         c.data, c.strides[0]
+//     );
+//     axm::op::sgemm_naive(
+//         M, N, K,
+//         alpha,
+//         a.data, a.strides[0], false,
+//         b.data, b.strides[0], false,
+//         beta,
+//         d.data, d.strides[0]
+//     );
 
-    std::cout << a << "\n\n";
-    std::cout << b << "\n\n";
-    std::cout << c << "\n\n";
-    std::cout << d << "\n\n";
-}
+//     std::cout << a << "\n\n";
+//     std::cout << b << "\n\n";
+//     std::cout << c << "\n\n";
+//     std::cout << d << "\n\n";
+// }
