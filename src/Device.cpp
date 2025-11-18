454ed3f9146d4246cf2d10a4f2a04579b0d09e6c
DavidGmc18 <davidgames.youtub@gmail.com>
2025-11-18 20:33:07 +0100

    Refactor

--- src/Device.cpp
+++ src/Device.cpp
@@ -1,9 +1,13 @@
 #include "AXM/Device.hpp"
 
-std::ostream& axm::operator<<(std::ostream& os, Device device) {
+namespace axm {
+
+std::ostream& operator<<(std::ostream& os, Device device) {
     switch(device) {
         case CPU:  return os << "CPU";
         case CUDA: return os << "CUDA";
         default:   return os << "UNKNOWN";
     }
+}
+
 }
